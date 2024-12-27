b8ad6b82d975303bcca2e62616fbc1a60f67ef46
Samir P Salim <samirpsalim.git@gmail.com>
2024-12-27 16:47:45 +0800

    Updated the UI.

--- numeral-convertor/calculator.js
+++ numeral-convertor/calculator.js
@@ -1,5 +1,6 @@
 function GetChineseNumber()
 {
-    var number= document.getElementById("input");
+    var number= document.getElementById("input").innerHTML;
     document.getElementById("output").innerHTML = number;
+    document.getElementById("output").style.display="block";
 }
